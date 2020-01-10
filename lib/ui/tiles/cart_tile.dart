dde54c571f8e144cd526b1b04094b9878cc3271b
Leandro Araujo <lean.ib@hotmail.com>
2020-01-10 16:10:30 -0300

    Login, Home, Lista de Categorias, lista d eprodutos, detalhes, carrinho

--- lib/ui/tiles/cart_tile.dart
+++ lib/ui/tiles/cart_tile.dart
@@ -4,31 +4,64 @@ import 'package:confectstore/data/product_data.dart';
 import 'package:flutter/material.dart';
 
 class CartTile extends StatelessWidget {
-
   final CartProduct cartProduct;
 
   CartTile(this.cartProduct);
 
   @override
   Widget build(BuildContext context) {
-
-
-    Widget _buildContent(){
+    Widget _buildContent() {
       return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.all(8.0),
-            width: 120.0,
-            child: Image.network(cartProduct.productData.images[0], fit: BoxFit.cover)
-          ),
+              padding: EdgeInsets.all(8.0),
+              width: 120.0,
+              child: Image.network(cartProduct.productData.images[0],
+                  fit: BoxFit.cover)),
           Expanded(
             child: Container(
               padding: EdgeInsets.all(8.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Text(
                     cartProduct.productData.title,
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 17.0),
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 17.0),
+                  ),
+                  Text(
+                    "Tamanho: ${cartProduct.size}",
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    "R\$ ${cartProduct.productData.price.toStringAsFixed(2)}",
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment:  MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.remove),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {},
+                      ),
+                      Text(cartProduct.quantity.toString()),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        child: Text("Remover"),
+                        textColor: Colors.grey[500],
+                        onPressed: (){},
+                      )
+                    ],
                   )
                 ],
               ),
@@ -40,21 +73,29 @@ class CartTile extends StatelessWidget {
 
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      child: cartProduct.productData == null ? FutureBuilder<DocumentSnapshot>(
-        future: Firestore.instance.collection("products").document(cartProduct.category).collection("items").document(cartProduct.productId).get(),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            cartProduct.productData = ProductData.fromDocument(snapshot.data);
-            return _buildContent();
-          } else{
-            return Container(
-              height: 70,
-              child: CircularProgressIndicator(),
-              alignment: Alignment.center,
-            );
-          }
-        },
-      ) : _buildContent(),
+      child: cartProduct.productData == null
+          ? FutureBuilder<DocumentSnapshot>(
+              future: Firestore.instance
+                  .collection("products")
+                  .document(cartProduct.category)
+                  .collection("itens")
+                  .document(cartProduct.productId)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  cartProduct.productData =
+                      ProductData.fromDocument(snapshot.data);
+                  return _buildContent();
+                } else {
+                  return Container(
+                    height: 70,
+                    child: CircularProgressIndicator(),
+                    alignment: Alignment.center,
+                  );
+                }
+              },
+            )
+          : _buildContent(),
     );
   }
 }
